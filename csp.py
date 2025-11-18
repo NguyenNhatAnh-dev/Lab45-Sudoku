b4c208553453c5b4e20caa573a9d55b2ddb01cef
Nguyen Nhat Anh <nhatanh4works@gmail.com>
2025-11-18 22:57:20 +0700

    Update code

--- csp.py
+++ csp.py
@@ -7,13 +7,34 @@ class csp:
     # INITIALIZING THE CSP
     def __init__(self, domain=digits, grid=""):
         """
-        Unitlist consists of the 27 lists of peers
-        Units is a dictionary consisting of the keys and the corresponding lists of peers
-        Peers is a dictionary consisting of the 81 keys and the corresponding set of 27 peers
-        Constraints denote the various all-different constraints between the variables
+        Initializes the CSP model for Sudoku.
         """
-        "***YOUR CODE HERE ***"
+        
+        rows = 'ABCDEFGHI'
+        cols = '123456789'
+        
+        def cross(A, B):
+            return [a + b for a in A for b in B]
 
+        self.variables = cross(rows, cols) 
+
+        row_units = [cross(r, cols) for r in rows]
+        col_units = [cross(rows, c) for c in cols]
+        square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+        
+        self.unitlist = row_units + col_units + square_units 
+
+        self.units = dict((s, [u for u in self.unitlist if s in u]) for s in self.variables)
+
+        self.peers = dict((s, set(sum(self.units[s], [])) - {s}) for s in self.variables)
+        
+
+        self.values = self.getDict(grid)
+        
+        self.constraints = []
+        for v1 in self.variables:
+            for v2 in self.peers[v1]:
+                self.constraints.append((v1, v2))
 
 
     def getDict(self, grid=""):
--- search.py
+++ search.py
@@ -6,7 +6,7 @@ for solving Sudoku problem which is called by sudoku.py
 from csp import *
 from copy import deepcopy
 import util
-
+from util import digits, rows, cols, squares 
 
 
 def Backtracking_Search(csp):
@@ -14,73 +14,116 @@ def Backtracking_Search(csp):
     Backtracking search initialize the initial assignment
     and calls the recursive backtrack function
     """
-    "***YOUR CODE HERE ***"
+    assignment = {}
+    for var in csp.variables:
+        if len(csp.values[var]) == 1:
+            assignment[var] = csp.values[var]
+            
+    csp.values = deepcopy(csp.values)
 
-    util.raiseNotDefined()
+    return Recursive_Backtracking(assignment, csp)
 
 
 def Recursive_Backtracking(assignment, csp):
     """
     The recursive function which assigns value using backtracking
     """
-    "***YOUR CODE HERE ***"
+    
+    if len(assignment) == len(csp.variables):
+        return assignment
 
-    util.raiseNotDefined()
+    try:
+        var = Select_Unassigned_Variables(assignment, csp)
+    except ValueError:
+        return None 
+    except:
+        return assignment 
+
+    for value in Order_Domain_Values(var, assignment, csp):
+        
+        if isConsistent(var, value, assignment, csp):
+            
+            state_backup = deepcopy(csp.values)
+
+            assignment[var] = value
+
+            inferences = {} 
+            inference_status = Inference(assignment, inferences, csp, var, value)
+            
+            if inference_status is not False: 
+
+                result = Recursive_Backtracking(assignment, csp)
+                
+                if result is not None:
+                    return result
+
+            del assignment[var]
+    
+            csp.values = state_backup
+            
+    return None
+
 
 def Inference(assignment, inferences, csp, var, value):
     """
-    Forward checking using concept of Inferences
+    Forward checking (FC) implementation.
+    Prunes 'value' from the domains of all unassigned peers of 'var'.
+    Includes recursive propagation if a domain shrinks to size 1.
+    
+    Returns False if a domain becomes empty (FAILURE), True otherwise (SUCCESS).
     """
-
-    inferences[var] = value
-
+    
     for neighbor in csp.peers[var]:
         if neighbor not in assignment and value in csp.values[neighbor]:
-            if len(csp.values[neighbor]) == 1:
-                return "FAILURE"
 
-            remaining = csp.values[neighbor] = csp.values[neighbor].replace(value, "")
+            if len(csp.values[neighbor]) == 1 and csp.values[neighbor] == value:
+                return False
+
+            remaining = csp.values[neighbor].replace(value, "")
+            csp.values[neighbor] = remaining
+
+            if len(remaining) == 0:
+                return False
 
             if len(remaining) == 1:
-                flag = Inference(assignment, inferences, csp, neighbor, remaining)
-                if flag == "FAILURE":
-                    return "FAILURE"
+                flag = Inference(assignment, inferences, csp, neighbor, remaining[0])
+                if flag is False:
+                    return False
 
-    return inferences
+    return True
 
 def Order_Domain_Values(var, assignment, csp):
     """
-    Returns string of values of given variable
+    Returns string of values of given variable (simple ordering).
     """
     return csp.values[var]
 
 def Select_Unassigned_Variables(assignment, csp):
     """
-    Selects new variable to be assigned using minimum remaining value (MRV)
+    Selects new variable to be assigned using minimum remaining value (MRV).
     """
-    unassigned_variables = dict((squares, len(csp.values[squares])) for squares in csp.values if squares not in assignment.keys())
+    unassigned_variables = {}
+    
+    for var in csp.variables:
+        if var not in assignment:
+            unassigned_variables[var] = len(csp.values[var])
+    
+    if not unassigned_variables:
+        raise ValueError("No unassigned variables found, but assignment is incomplete.")
+
     mrv = min(unassigned_variables, key=unassigned_variables.get)
     return mrv
 
-def isComplete(assignment):
-    """
-    Check if assignment is complete
-    """
-    return set(assignment.keys()) == set(squares)
-
 def isConsistent(var, value, assignment, csp):
     """
-    Check if assignment is consistent
+    Check if assignment is consistent (i.e., 'value' does not conflict 
+    with any assigned peers of 'var').
     """
     for neighbor in csp.peers[var]:
         if neighbor in assignment.keys() and assignment[neighbor] == value:
             return False
     return True
 
-def forward_checking(csp, assignment, var, value):
-    csp.values[var] = value
-    for neighbor in csp.peers[var]:
-        csp.values[neighbor] = csp.values[neighbor].replace(value, '')
 
 def display(values):
     """
@@ -102,5 +145,43 @@ def write(values):
     """
     output = ""
     for variable in squares:
-        output = output + values[variable]
+        output = output + values[variable] 
     return output
+
+
+def AC3(csp):
+    """
+    AC-3 algorithm: enforce arc consistency.
+    """
+    queue = list(csp.constraints)  
+
+    while queue:
+        Xi, Xj = queue.pop(0)
+
+        if REVISE(csp, Xi, Xj):
+            if len(csp.values[Xi]) == 0:
+                return False
+            for Xk in csp.peers[Xi]:
+                if Xk != Xj:
+                    queue.append((Xk, Xi))
+
+    return True
+
+
+def REVISE(csp, Xi, Xj):
+    """
+    Make Xi arc-consistent with Xj.
+    Remove any value x in domain(Xi) that has no possible support in domain(Xj).
+    Constraint: Xi != Xj  (Sudoku rule)
+    """
+    revised = False
+    domain_Xi = csp.values[Xi]
+
+    for x in domain_Xi[:]:  
+        has_support = any(y != x for y in csp.values[Xj])
+
+        if not has_support:
+            csp.values[Xi] = csp.values[Xi].replace(x, '')
+            revised = True
+
+    return revised
